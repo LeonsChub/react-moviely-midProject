614d138f5dc4ce4e9817066e674e2503d3492d9e
Leon <pavlenkoleon@gmail.com>
2022-12-07 14:47:41 +0200

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Nav } from 'react-bootstrap'
+import Button from 'react-bootstrap/Button'
+import NavHead from './assets/components/Navbar/NavHead'
+
+
+function App() {
+  return (
+    <div className="App">
+      <NavHead />
+    </div>
+  )
+}
+
+export default App
